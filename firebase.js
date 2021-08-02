26438314efcfd3198a11e0db91dac9515d60a4a7
yosiaagustadewa <yosiaagustadewa@gmail.com>
2021-08-02 22:12:42 +0700

    - update firebase config

--- firebase.js
+++ firebase.js
@@ -1,10 +1,18 @@
 import firebase from 'firebase'
-import * as firebaseConfig from './firebase.config.json';
+// import * as firebaseConfig from './firebase.config.json';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseApp = !firebase.apps.length ?
-    firebase.initializeApp(firebaseConfig) :
+    firebase.initializeApp({
+      apiKey: process.env.apiKey,
+      authDomain: process.env.authDomain,
+      projectId: process.env.projectId,
+      storageBucket: process.env.storageBucket,
+      messagingSenderId: process.env.messagingSenderId,
+      appId: process.env.appId,
+      measurementId: process.env.measurementId
+    }) :
     firebase.app();
 
 const db = firebaseApp.firestore();
